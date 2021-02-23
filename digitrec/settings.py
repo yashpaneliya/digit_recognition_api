bc9653ef03d0078d9ae0a89e4a2d57b548897f56
Yash Paneliya <yashsp1158@gmail.com>
2021-02-23 09:55:28 +0530

    Minor changes

--- digitrec/settings.py
+++ digitrec/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
