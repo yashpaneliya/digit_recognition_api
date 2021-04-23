3355b04f4f159bd5059106b7049bdd9ddce15816
Yash Paneliya <yashsp1158@gmail.com>
2021-04-23 12:19:11 +0530

    Update settings.py

--- digitrec/settings.py
+++ digitrec/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 # PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
