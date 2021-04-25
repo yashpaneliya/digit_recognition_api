c86f54d0e66e4576357826d8c5ecb04d7831e9e9
Yash Paneliya <yashsp1158@gmail.com>
2021-04-25 15:56:15 +0530

    Heroku app changed

--- digitrec/settings.py
+++ digitrec/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # # STATIC_URL = '/static/'
 
