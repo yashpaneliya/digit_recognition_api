eafcae5d75f6f76617d8aff33d4d314f2376f813
Yash Paneliya <yashsp1158@gmail.com>
2021-04-25 15:55:05 +0530

    Heroku app changed

--- digitrec/settings.py
+++ digitrec/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # # STATIC_URL = '/static/'
 
 # # Extra lookup directories for collectstatic to find static files
